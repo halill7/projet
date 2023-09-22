eac228dba02a37b334ecab961d81874259187819
duran <duranhalil82@gmail.com>
2023-09-22 17:58:24 +0200

    Dossier

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {HttpExceptionFilter} from "./config/http-exception.filter";
+import {HttpExceptionFilter} from "@common/config/exception/http-exception.filter";
 
 async function bootstrap() {
 
